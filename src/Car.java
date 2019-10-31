5a2de39bf5ec7fad7f5f039e69e36d6b785757b3
Sam Flavin <flavi022@d.umn.edu>
2019-10-31 15:23:08 -0500

    init commit

---
+++ src/Car.java
@@ -0,0 +1,13 @@
+
+public class Car {
+
+    public String model;
+    public String color;
+    public int seats;
+
+    public void display() {
+        System.out.println("model is : " + model);
+    }
+}
+
+
---
+++ src/ClassesMain.java
@@ -0,0 +1,17 @@
+
+public class ClassesMain {
+    public static void main(String[] args) {
+        Car ferrari = new Car();
+        ferrari.model = "Ferrari F430";
+        ferrari.display();
+
+        Car audi = new Car();
+        audi.model = "TT";
+        audi.display();
+
+        Car toyota = new Car();
+        toyota.model = "Land Cruiser";
+        toyota.display();
+    }
+}
+
