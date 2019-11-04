ec74c887d5324a4b70133c08cf20a4a86638cc55
Sam Flavin <flavi022@d.umn.edu>
2019-11-03 22:56:55 -0600

    diff types, order, num

--- src/MethodOverloading.java
+++ src/MethodOverloading.java
@@ -8,6 +8,22 @@ public class MethodOverloading {
         System.out.println("integer arguments:" + i);
     }
 
+    public void method1(float i) {
+        System.out.println("float arguments:" + i);
+    }
+
+    public void method1(int i, int j) {
+        System.out.println("integer arguments:" + i + ',' + j);
+    }
+
+    public void method1(int i, float j) {
+        System.out.println("integer arguments:" + i + ',' + j);
+    }
+
+    public void method1(float i, int j) {
+        System.out.println("integer arguments:" + i + ',' + j);
+    }
+
     public static void main(String[] args) {
         MethodOverloading methodOverloading = new MethodOverloading();
         methodOverloading.method1();
