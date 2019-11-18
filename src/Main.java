3350f0a05d902e07bd4e592e8bab5f1cca29cba7
Sam Flavin <flavi022@d.umn.edu>
2019-11-18 16:13:33 -0600

    new thread implementation

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-
 public class Main {
 
 
@@ -8,8 +7,12 @@ public class Main {
             public void run() {
                 // Code that will run new thread
                 System.out.println("We are in a new thread " + Thread.currentThread().getName() );
+                System.out.println("Current thread priority is " + Thread.currentThread().getPriority());
             }
         });
+       thread.setName("New Worker Thread");
+
+       thread.setPriority(Thread.MAX_PRIORITY);
         System.out.println("We are in thread: " + Thread.currentThread().getName() + "before starting a new thread");
         thread.start();
         System.out.println("We are in thread: " + Thread.currentThread().getName() + "after starting a new thread");
