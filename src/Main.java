003a21981698800fab1f3cd4b429d3c99093130e
Sam Flavin <flavi022@d.umn.edu>
2019-11-18 21:12:43 -0600

    hacker multithreading example

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,105 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
+    public static final int MAX_PASSWORD = 9999;
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        Vault vault = new Vault(random.nextInt(MAX_PASSWORD));
+         List<Thread> threads = new ArrayList<>();
+
+        threads.add(new AscendingHackerThread(vault));
+        threads.add(new DescendingHackerThread(vault));
+        threads.add(new PoliceThread());
+
+        for(Thread thread : threads) {
+            thread.start();
+        }
+    }
 
 
-    public static void main(String[] args) throws InterruptedException {
-       Thread thread = new Thread(new Runnable(){
-            @Override
-            public void run() {
-                // Code that will run new thread
-                System.out.println("We are in a new thread " + Thread.currentThread().getName() );
-                System.out.println("Current thread priority is " + Thread.currentThread().getPriority());
+
+
+    private static class Vault {
+        private int password;
+        public Vault(int password){
+            this.password = password;
+        }
+
+        public boolean isCorrectPassword(int guess) {
+            try {
+                Thread.sleep(5);
+            } catch (InterruptedException e) {
             }
-        });
-       thread.setName("New Worker Thread");
+            return this.password == guess;
+        }
+    }
 
-       thread.setPriority(Thread.MAX_PRIORITY);
-        System.out.println("We are in thread: " + Thread.currentThread().getName() + "before starting a new thread");
-        thread.start();
-        System.out.println("We are in thread: " + Thread.currentThread().getName() + "after starting a new thread");
+    private static abstract class HackerThread extends Thread {
+        protected Vault vault;
 
-        Thread.sleep( 10000);
+        public HackerThread(Vault vault) {
+            this.vault = vault;
+            this.setName(this.getClass().getSimpleName());
+            this.setPriority(Thread.MAX_PRIORITY);
+        }
+
+        @Override
+        public void start() {
+            System.out.println("Starting thread " + this.getName());
+            super.start();
+        }
+    }
+
+    private static class AscendingHackerThread extends HackerThread {
+            public AscendingHackerThread (Vault vault) {
+                super (vault);
+            }
+
+            @Override
+            public void run () {
+                for( int guess = 0 ; guess < MAX_PASSWORD ; guess++) {
+                    if (vault.isCorrectPassword(guess)) {
+                        System.out.println(this.getName() + "guessed the passwords " + guess);
+                        System.exit(0);
+                    }
+                }
+            }
+
+    }
+
+    private static class DescendingHackerThread extends HackerThread {
+
+        public DescendingHackerThread(Vault vault) {
+            super(vault);
+        }
+
+        @Override
+        public void run() {
+         for( int guess = MAX_PASSWORD ; guess >= 0 ; guess-- ){
+             if(vault.isCorrectPassword(guess)) {
+                 System.out.println(this.getName() + " guessed the password " + guess);
+                 System.exit(0);
+             }
+         }
+        }
+    }
+
+    private static class PoliceThread extends Thread {
+        @Override
+        public void run() {
+            for(int i = 10 ; i > 0 ; i --) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                }
+                System.out.println(i);
+            }
+            System.out.println("Game over for you hackers");
+            System.exit(0);
+        }
     }
 }
